1431396bdeb00e7a43556d9f02206c7f4fc6fe59
acvasniuc <andrei.a.cvasniuc@gmail.com>
2021-08-26 19:11:51 +0300

    added grpc server and client

---
+++ solution/GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var greeter = new Greeter.GreeterClient(channel);
+            var reply = greeter.SayHello(new HelloRequest { Name = "Світ", Language = "ua-UA" });
+            var reply2 = greeter.SayHello(new HelloRequest { Name = "World" });
+            Console.WriteLine(reply.Message);
+            Console.WriteLine(reply2.Message);
+        }
+    }
+}
